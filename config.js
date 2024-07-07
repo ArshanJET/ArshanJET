c4c6be13c870386641f1e4fbd06c544f337f80d1
ArshanJET <87538569+ArshanJET@users.noreply.github.com>
2024-07-07 22:39:36 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,29 @@
+const discord = require("discord.js-selfbot-v13");
+
+function reloadPresence(client) {
+    // Ensure global.startTime is set
+    if (!global.startTime) {
+        global.startTime = Date.now();
+    }
+
+    // Create the RichPresence object
+    const activity = new discord.RichPresence()
+        .setApplicationId("1256607227444330606") // Replace with your own bot application id
+        .setType("WATCHING") // Adjust based on the activity type you want
+        .setName("Original Gangster Community")
+        .setDetails("OGC")
+        .setState("Grinding...") // Set the activity state
+        .setStartTimestamp(global.startTime) // Make sure global.startTime is defined correctly
+        .setAssetsLargeImage("https://cdn.discordapp.com/attachments/1248627708100088001/1259528286523097202/Thanos.gif?ex=668c02a0&is=668ab120&hm=0be2791ec8b5a651857624b2b94a8dbcae8c9179c03dc0ec22a31f9cf7a3c164&") // Replace with your large image key
+        .setAssetsLargeText("Watching OGC")
+        .addButton('Join OGC', "https://app.ogcom.xyz/invite/FywvD4zZwISY-qNi")
+        .addButton('Zealy', "https://zealy.io/cw/web3epicchallenge/invite/Le-o0OQfKd2dGs4Nia-Bt");
+
+    // Set the activity and status
+    client.user.setPresence({
+        status: "online", // Set bot status: online, idle, dnd, invisible
+        activities: [activity.toJSON()]
+    });
+}
+
+module.exports = reloadPresence;
