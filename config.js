b25285e0c95f6f8bbfc4d7afa95284bd8a061558
ArshanJET <87538569+ArshanJET@users.noreply.github.com>
2024-07-08 03:01:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,11 +11,11 @@ function reloadPresence(client) {
         .setApplicationId("1256607227444330606") // Replace with your own bot application id
         .setType("WATCHING") // Adjust based on the activity type you want
         .setName("Original Gangster Community")
-        .setDetails("OGC")
-        .setState("Grinding...") // Set the activity state
+        .setDetails("Grinding OGC")
+        .setState("...") // Set the activity state
         .setStartTimestamp(global.startTime) // Make sure global.startTime is defined correctly
         .setAssetsLargeImage("https://cdn.discordapp.com/attachments/1248627708100088001/1259528286523097202/Thanos.gif?ex=668c02a0&is=668ab120&hm=0be2791ec8b5a651857624b2b94a8dbcae8c9179c03dc0ec22a31f9cf7a3c164&") // Replace with your large image key
-        .setAssetsLargeText("Watching OGC")
+        .setAssetsLargeText("Join below")
         .addButton('Join OGC', "https://app.ogcom.xyz/invite/FywvD4zZwISY-qNi")
         .addButton('Zealy', "https://zealy.io/cw/web3epicchallenge/invite/Le-o0OQfKd2dGs4Nia-Bt");
 
